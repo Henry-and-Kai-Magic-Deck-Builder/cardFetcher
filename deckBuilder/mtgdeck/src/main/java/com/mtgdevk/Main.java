04695b68901a209c0c2a0955610c0481744a0fe2
sippery <henrymarrone@gmail.com>
2024-05-14 10:25:30 -0400

    Changed the output to use JOptionPane

--- deckBuilder/mtgdeck/src/main/java/com/mtgdevk/Main.java
+++ deckBuilder/mtgdeck/src/main/java/com/mtgdevk/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import io.magicthegathering.javasdk.api.*;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,13 +14,18 @@ public class Main {
         Card chosenCard;
         Scanner userInput = new Scanner(System.in); 
         Scanner cardChoice = new Scanner(System.in);
+        String input;
+        String filter;
+        int i = JOptionPane.YES_NO_OPTION;
+        String placeHolder;
+
+        int finalChoice;
         cardPrinter printer = new cardPrinter();
         boolean run = true;
-        int i;
         while(run){
                 //take user input to get card name
-            System.out.println("Enter a MTG card name: ");
-            String filter = "name=" + userInput.next();
+            input = JOptionPane.showInputDialog("Enter a MTG card name: ");
+            filter = "name=" + input;
             //fetch all cards with the inputted name from the API, catching for errors in doing so
             //printerLoop(Cards card){}
             try{
@@ -40,14 +46,20 @@ public class Main {
                 if(counter < 5){
                 printer.printCard(c);
                 counter++;
+                //placeHolder = placeHolder +"\n"+printer.printCard(c);
+                ;
                 }
             }
+            //JOptionPane.showMessageDialog(null, "Oh Hello.." +name);
             //user choses which of the top 5 to chose from
+            /*
+             *
+             
             System.out.println("Which card would you like in your deck? (1-5, enter 0 to end)");
-            int finalChoice = cardChoice.nextInt();
+            finalChoice = cardChoice.nextInt();
             //list selects from 0-4, so 1 is subtracted from the variable
             finalChoice--;
-            if( finalChoice == -1){
+            if(finalChoice == -1){
             }
             else{
                 //add the chosen card to a list variable called deck
@@ -55,10 +67,16 @@ public class Main {
                 deck.add(chosenCard);
                 System.out.println(deck);
             }
+            */
+            chosenCard = cards.get(0);
+                deck.add(chosenCard);
+                System.out.println(deck);
             //allows the user to quit
-            System.out.print("Would you like to keep adding to your deck? Yes: [1] No: [2]\n");
-            i = userInput.nextInt();
-            if (i == 2){
+            if (JOptionPane.showConfirmDialog(null, "Would you like to keep adding to your deck?", "WARNING",
+                JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+            // yes option
+            } else {
+            // no option
                 run = false;
             }
         }
